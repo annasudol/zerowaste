76fc05dea2dc9be5be5b57a7379a7cad47fec072
anna <sudolann@gmail.com>
2020-05-04 21:13:29 +0200

    added prisma bindings

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -2,11 +2,40 @@ import { Prisma } from 'prisma-binding';
 const prisma = new Prisma({
     typeDefs: 'src/generated/schema.graphql',
     endpoint: 'http://localhost:4466',
-    secret: 'my-super-secret-secret'
 })
 
+
+const createRecipeForUser = async (authorId, data) => {
+    const userExists = await prisma.exists.User({ id: authorId })
+
+    if (!userExists) {
+        throw new Error('User not found')
+    }
+
+    const recipe = await prisma.mutation.createRecipe({
+        data: {
+            ...data,
+            author: {
+                connect: {
+                    id: authorId
+                }
+            }
+        }
+    }, '{ author { id name email recipes { id title description } } }')
+
+    return recipe
+}
+createRecipeForUser("ck9sgen4y008c078218f6n1h4", {
+    title: "exaple",
+    description: "ex",
+    prep: 10,
+    cook: 10
+
+}).then((user) => {
+    console.log(JSON.stringify(user))
+}).catch(err => console.warn(err, "err"))
 // // Retrieve `name` of a specific user
-// prisma.query.user(null, '{id name email recipes { id }}').then(data => console.log(data, "data"))
+// prisma.query.users(null, '{id name email recipes { id }}').then(data => console.log(data, "data"))
 
 // // Retrieve `id` and `name` of all users
 // prisma.query.users(null, '{ id name }')
