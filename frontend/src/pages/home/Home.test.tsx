cd83f8c01c1c0960a29846345166160c5a52673c
anna <sudolann@gmail.com>
2020-06-22 15:44:17 +0200

    home unit test

--- frontend/src/pages/home/Home.test.tsx
+++ frontend/src/pages/home/Home.test.tsx
@@ -3,14 +3,11 @@ import React from 'react';
 import DeleteIcon from '@material-ui/icons/Delete';
 import { Home } from './Home';
 import toJson from 'enzyme-to-json';
-import { act } from "react-dom/test-utils";
 
 describe('component', (): void => {
   describe('Home', (): void => {
         it('match snapshot', (): void => {
-        const wrapper = shallow(
-            <Home />,
-        );
+        const wrapper = shallow(<Home />);
 
         expect(toJson(wrapper)).toMatchSnapshot();
         });
