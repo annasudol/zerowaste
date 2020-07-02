efd719ad9c7550502e9168bbc61b83d100f5ecbf
anna <sudolann@gmail.com>
2020-07-02 17:13:32 +0200

    Updates

---
+++ precache-manifest.c982bc379f5562ce89da39ecee21f277.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b1566b7150e683a4e400a7a4a8d0558",
+    "url": "/zerowaste/index.html"
+  },
+  {
+    "revision": "9e7c97ba52568cdb398a",
+    "url": "/zerowaste/static/css/2.919b51fa.chunk.css"
+  },
+  {
+    "revision": "848cf8f4dd45d3f855ae",
+    "url": "/zerowaste/static/css/main.9ec51c13.chunk.css"
+  },
+  {
+    "revision": "9e7c97ba52568cdb398a",
+    "url": "/zerowaste/static/js/2.ccb91f66.chunk.js"
+  },
+  {
+    "revision": "a41c879f8bdb54796b2468bd148c705b",
+    "url": "/zerowaste/static/js/2.ccb91f66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "848cf8f4dd45d3f855ae",
+    "url": "/zerowaste/static/js/main.e8d41f18.chunk.js"
+  },
+  {
+    "revision": "8cd2c687faa857e642d8",
+    "url": "/zerowaste/static/js/runtime-main.e26679a6.js"
+  },
+  {
+    "revision": "54c80e4045a821f3428002fee660b4ec",
+    "url": "/zerowaste/static/media/technology.54c80e40.svg"
+  }
+]);
